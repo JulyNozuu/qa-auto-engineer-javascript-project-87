e693cad3d218400fa9b40c9e37dc49310b6b0bf6
Выставкина Юлия <yulya.reso@mail.ru>
2025-03-07 21:31:15 +0300

    Update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 program.parse();
