369bbca84bc69279fe3f44904b19d23980bdb23d
Выставкина Юлия <yulya.reso@mail.ru>
2025-03-07 17:39:30 +0300

    Add help info

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+const { Command } = require('commander');
+const program = new Command();
 
-console.log('hello');
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0');
+program.parse();
