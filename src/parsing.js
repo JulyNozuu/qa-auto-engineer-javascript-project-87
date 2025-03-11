b920bb44895b5b639c248c7e157c6939c75f3db6
Выставкина Юлия <yulya.reso@mail.ru>
2025-03-11 22:27:00 +0300

    update gendiff

--- src/parsing.js
+++ src/parsing.js
@@ -1,11 +1,15 @@
 import * as fs from "node:fs";
+import path from "node:path";
 import { cwd } from 'node:process';
 
 const parsing = (args) => {
+    if (args[0] == '.'){
+        args = path.resolve(process.cwd(), args);
+    }
 const json1 = fs.readFileSync(args);
 return JSON.parse(json1);
 }
+    
 export default parsing;
 
-//console.log (parsing('/Users/july/Desktop/file.json'));
-//console.log(`Current directory: ${cwd()}`);
+//console.log (parsing('./Desktop/file2.json'));
