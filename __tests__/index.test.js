3de843e754f632a15328065fae63b083964d1b73
Выставкина Юлия <yulya.reso@mail.ru>
2025-03-18 18:21:45 +0300

    Add check YAMl

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,19 @@
 import diff from '../src/diff.js';
 import { readFileSync } from 'node:fs';
 import * as fs from 'node:fs';
-import path from 'node:path';
+import yaml from 'js-yaml';
 
 const pathJSON1 = fs.readFileSync(new URL('__fixtures__/test1.json', import.meta.url));
 const pathJSON2 = fs.readFileSync(new URL('__fixtures__/test2.json', import.meta.url));
 const pathResult = fs.readFileSync(new URL('__fixtures__/result.txt', import.meta.url), 'utf8');
+const YAMl1 = fs.readFileSync(new URL('__fixtures__/test1yml.yml', import.meta.url), 'utf8');
+const YAMl2 = fs.readFileSync(new URL('__fixtures__/test2yml.yml', import.meta.url), 'utf8');
 console.log(pathResult);
 
 test('checkJSON', () => {
     expect(diff(JSON.parse(pathJSON1),JSON.parse(pathJSON2))).toEqual(pathResult);
   });
 
-  
+test('checkYAML', () => {
+  expect(diff(yaml.load(YAMl1),yaml.load(YAMl2))).toEqual(pathResult);
+});
--- src/parsing.js
+++ src/parsing.js
@@ -1,12 +1,19 @@
 import * as fs from 'node:fs';
 import path from 'node:path';
+import yaml from 'js-yaml';
 
 const parsing = (args) => {
   if (args[0] === '.') {
     args = path.resolve(process.cwd(), args);
   }
-  const json1 = fs.readFileSync(args);
-  return JSON.parse(json1);
+  
+  if (args.slice(-4) == 'json'){
+    const file = fs.readFileSync(args);
+    return JSON.parse(file)};
+
+  if (args.slice(-3) == 'yml'){
+    return yaml.load(fs.readFileSync(args, 'utf8'));};
 };
 
 export default parsing;
+
