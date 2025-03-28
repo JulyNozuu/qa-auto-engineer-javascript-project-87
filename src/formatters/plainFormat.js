2c500d81dca2869ae2bae1c3961643d2d9d76156
Выставкина Юлия <yulya.reso@mail.ru>
2025-03-28 14:15:12 +0300

    fix plain formate

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -6,7 +6,7 @@ const plainFormat = (tree) => {
     } else if (element.action == "updated") {
       result = `${result}Property '${element.key}' was updated. From ${element.obj1} to ${element.obj2}\n`;
     } else if (element.action == "added") {
-      result = `${result}Property '${element.key}' was added with value: ${element.obj} `;
+      result = `${result}Property '${element.key}' was added with value: ${element.obj}`;
     }
   }
   return result;
