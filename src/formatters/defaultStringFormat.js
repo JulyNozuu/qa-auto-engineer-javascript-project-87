6cb727ffb18f0cc2ecae638ddcbbe0977e7b06c5
Выставкина Юлия <yulya.reso@mail.ru>
2025-03-28 14:10:25 +0300

    Fix stringformate yml

--- src/formatters/defaultStringFormat.js
+++ src/formatters/defaultStringFormat.js
@@ -4,13 +4,13 @@ const defaultStringFormat = (tree) => {
   let result = "";
   for (const element of tree) {
     if (element.action == "removed") {
-      result = `${result}\n  - ${element.key}: ${element.obj} `;
+      result = `${result}\n  - ${element.key}: ${element.obj}`;
     } else if (element.action == "mapped") {
-      result = `${result}\n    ${element.key}: ${element.obj} `;
+      result = `${result}\n    ${element.key}: ${element.obj}`;
     } else if (element.action == "updated") {
-      result = `${result}\n  - ${element.key}: ${element.obj1}\n  + ${element.key}: ${element.obj2} `;
+      result = `${result}\n  - ${element.key}: ${element.obj1}\n  + ${element.key}: ${element.obj2}`;
     } else if (element.action == "added") {
-      result = `${result}\n  + ${element.key}: ${element.obj} `;
+      result = `${result}\n  + ${element.key}: ${element.obj}`;
     }
   }
   return `{${result}\n}`;
