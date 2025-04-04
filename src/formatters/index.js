ee39a8921cdb99def900a7ce278c6eacfd78a75d
Выставкина Юлия <yulya.reso@mail.ru>
2025-04-04 15:24:35 +0300

    fix formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import plainFormat from './plainFormat.js';
 import JSONFormat from './JSONFormate.js';
 import defaultStringFormat from './defaultStringFormat.js';
@@ -9,9 +10,11 @@ const formatters = {
 };
 
 const diffFormat = (tree, type) => {
-  if (!_.includes(formatters, type)) { return Error(`Unknown type: ${prop.type}`); }
   if (type == null) {
     return formatters.default(tree);
+  }
+  if (!_.includes(Object.keys(formatters), type)) {
+    return Error(`Unknown type: ${type}`);
   }
   return formatters[type.toLowerCase()](tree);
 };
